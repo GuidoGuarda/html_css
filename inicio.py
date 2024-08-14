d82b29ed35aada009be261e9755a2204b19c8db0
Italo <dcguidosenai@gmail.com>
2024-08-14 14:17:28 -0300

    timefut

--- inicio.py
+++ inicio.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template #rota do site, renderização
 from matematica import Matematica
+from timefut import TimeFut
+
 app = Flask(__name__)
 
 @app.route('/inicio')
@@ -15,5 +17,13 @@ def calcular():
     mat = Matematica(9,9)
     resposta = mat.somar()
     return render_template('calculo.html', resultado=resposta)
+
+@app.route("/listatimes")
+def listar_times():
+        t1 = TimeFut('Palmeiras',10)
+        t2 = TimeFut('Botafogo',7)
+        t3 = TimeFut('Flamengo',9)
+        lista = [t1,t2,t3]
+        return render_template('listatimes.html',times=lista)
     
-app.run()
+app.run(debug=True)
--- times.py
+++
@@ -1,5 +0,0 @@
-class Time():
-    def __init__(self,nome,vitorias):
-        self.nome = nome 
-        self.vitorias = vitorias
-
